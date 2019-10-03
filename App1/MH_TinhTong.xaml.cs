7895bf0b716dc7f28617c0793e362066c6397360
phanvantu1698 <56113243+phanvantu1698@users.noreply.github.com>
2019-10-03 17:07:01 +0700

    Add files via upload

---
+++ App1/MH_TinhTong.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MH_TinhTong : ContentPage
+    {
+        public MH_TinhTong()
+        {
+            InitializeComponent();
+        }
+
+        private void Cmdtrangchu_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Cmdtingtong_Clicked(object sender, EventArgs e)
+        {
+            double so1, so2, tong;
+            so1 = double.Parse(txtsoa.Text);
+            so2 = double.Parse(txtsob.Text);
+            tong = so1 + so2;
+            txttong.Text = tong.ToString();
+        }
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+          await Navigation.PushAsync(new MH_TinhTong());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+       void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NavigationPage(new MH_Chao()));
+        }
+
+       
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdAbsoluteLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdgirdLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Cmdlogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MH_Login());
+        }
+
+        private void CmdFlexLayout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdScrollView_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdEntry_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdListview1_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Chào", "Chào Bạn :" , "Thoát");
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
